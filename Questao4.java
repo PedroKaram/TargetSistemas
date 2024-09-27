86db76ae686eb5585b547c1350dcd95f5cb1d1a0
Pedro Karam Nöcker <pedrokaram4005@gmail.com>
2024-09-26 23:46:23 -0300

    Retirando comentários

--- Questao4.java
+++ Questao4.java
@@ -4,7 +4,6 @@ import java.util.Map;
 public class Questao4 {
 
     public static void main(String[] args) {
-        // Faturamento mensal por estado
         Map<String, Double> faturamentoPorEstado = new HashMap<>();
         faturamentoPorEstado.put("SP", 67836.43);
         faturamentoPorEstado.put("RJ", 36678.66);
@@ -12,14 +11,11 @@ public class Questao4 {
         faturamentoPorEstado.put("ES", 27165.48);
         faturamentoPorEstado.put("Outros", 19849.53);
 
-        // Calcular o total de faturamento
         double totalFaturamento = calcularTotalFaturamento(faturamentoPorEstado);
 
-        // Calcular e exibir percentual de representação
         exibirPercentuais(faturamentoPorEstado, totalFaturamento);
     }
 
-    // Método para calcular o total de faturamento
     public static double calcularTotalFaturamento(Map<String, Double> faturamento) {
         double total = 0;
         for (double valor : faturamento.values()) {
@@ -28,7 +24,6 @@ public class Questao4 {
         return total;
     }
 
-    // Método para calcular e exibir os percentuais
     public static void exibirPercentuais(Map<String, Double> faturamento, double total) {
         System.out.println("Percentual de representação do faturamento por estado:");
         for (Map.Entry<String, Double> entry : faturamento.entrySet()) {
